e8793177caec1af5c59202f2e8d44cfed3be5395
Vathsan <srivaths311@gmail.com>
2021-03-23 19:57:05 +0530

    basic functionality

--- DotaItemGuide/Home/views.py
+++ DotaItemGuide/Home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 
 from .models import Item
 
 def index(request):
-    return render(request, 'Home/index.html')
+    if request.method == 'POST':
+        query = request.POST.get('query')
+        q = Item.objects.filter(hint__icontains=query)
+        return render(request, 'Home/index.html',{'items': q})
+    else:
+        return render(request, 'Home/index.html')
